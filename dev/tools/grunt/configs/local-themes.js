c9e20166434345ebf0121bfaba8018d6b1b1bd7b
Oleksii Bezpoiasnyi <abezpoasnyj@gmail.com>
2020-11-03 09:24:39 +0200

    #4 Upgrade Magento & creating theme: configured files for Grunt

---
+++ dev/tools/grunt/configs/local-themes.js
@@ -0,0 +1,16 @@
+/**
+ * grunt exec:OleksiiBezpoiasnyi_luma_en_US && grunt less:OleksiiBezpoiasnyi_luma_en_US && grunt watch
+ */
+
+module.exports = {
+    OleksiiBezpoiasnyi_luma_en_US: {
+        area: 'frontend',
+        name: 'OleksiiBezpoiasnyi/luma',
+        locale: 'en_US',
+        files: [
+            'css/styles-m',
+            'css/styles-l'
+        ],
+        dsl: 'less'
+    },
+};
