718fec2f44d286c648beee844d39b9167f6c2add
Oleksii Bezpoiasnyi <abezpoasnyj@gmail.com>
2021-01-03 16:12:23 +0200

    #12 Model, Resource Model, Collection: added saving data to database in controller

--- app/code/OleksiiBezpoiasnyi/RegularCustomer/Controller/LoyaltyProgram/Registration.php
+++ app/code/OleksiiBezpoiasnyi/RegularCustomer/Controller/LoyaltyProgram/Registration.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace OleksiiBezpoiasnyi\RegularCustomer\Controller\LoyaltyProgram;
 
 use Magento\Framework\Controller\Result\Json as JsonResponse;
+use OleksiiBezpoiasnyi\RegularCustomer\Model\DiscountRequest;
 
 class Registration implements \Magento\Framework\App\Action\HttpPostActionInterface
 {
@@ -11,15 +12,50 @@ class Registration implements \Magento\Framework\App\Action\HttpPostActionInterf
      * @var \Magento\Framework\Controller\Result\JsonFactory $jsonFactory
      */
     private $jsonFactory;
+    /**
+     * @var \Magento\Framework\App\RequestInterface
+     */
+    private $request;
+    /**
+     * @var \OleksiiBezpoiasnyi\RegularCustomer\Model\DiscountRequestFactory
+     */
+    private $discountRequestFactory;
+    /**
+     * @var \Magento\Store\Model\StoreManagerInterface
+     */
+    private $storeManager;
+    /**
+     * @var \OleksiiBezpoiasnyi\RegularCustomer\Model\ResourceModel\DiscountRequest
+     */
+    private $discountRequestResource;
+    /**
+     * @var \Psr\Log\LoggerInterface
+     */
+    private $logger;
 
     /**
      * Controller constructor.
      * @param \Magento\Framework\Controller\Result\JsonFactory $jsonFactory
+     * @param \Magento\Framework\App\RequestInterface $request
+     * @param \OleksiiBezpoiasnyi\RegularCustomer\Model\DiscountRequestFactory $discountRequestFactory
+     * @param \Magento\Store\Model\StoreManagerInterface $storeManager
+     * @param \OleksiiBezpoiasnyi\RegularCustomer\Model\ResourceModel\DiscountRequest $discountRequestResource
+     * @param \Psr\Log\LoggerInterface $logger
      */
     public function __construct(
-        \Magento\Framework\Controller\Result\JsonFactory $jsonFactory
+        \Magento\Framework\Controller\Result\JsonFactory $jsonFactory,
+        \Magento\Framework\App\RequestInterface $request,
+        \OleksiiBezpoiasnyi\RegularCustomer\Model\DiscountRequestFactory $discountRequestFactory,
+        \Magento\Store\Model\StoreManagerInterface $storeManager,
+        \OleksiiBezpoiasnyi\RegularCustomer\Model\ResourceModel\DiscountRequest $discountRequestResource,
+        \Psr\Log\LoggerInterface $logger
     ) {
         $this->jsonFactory = $jsonFactory;
+        $this->request = $request;
+        $this->discountRequestFactory = $discountRequestFactory;
+        $this->storeManager = $storeManager;
+        $this->discountRequestResource = $discountRequestResource;
+        $this->logger = $logger;
     }
 
     /**
@@ -28,10 +64,23 @@ class Registration implements \Magento\Framework\App\Action\HttpPostActionInterf
     public function execute(): JsonResponse
     {
         $response = $this->jsonFactory->create();
-        $response->setData([
-            'message' => __('You request for register in Loyalty Program was successful!')
-        ]);
 
-        return $response;
+        try {
+            /** @var DiscountRequest $discountRequest */
+            $discountRequest = $this->discountRequestFactory->create();
+            $discountRequest->setName($this->request->getParam('name'))
+                ->setEmail($this->request->getParam('email'))
+                ->setWebsiteId($this->storeManager->getStore()->getWebsiteId())
+                ->setStatus(DiscountRequest::STATUS_PENDING);
+            $this->discountRequestResource->save($discountRequest);
+            $message = __('You request for registration in loyalty program was accepted! Your discount will be available after admin verification');
+        } catch (\Exception $e) {
+            $this->logger->error($e->getMessage());
+            $message = __('Your request can\'t be sent. Please, contact us if you see this message.');
+        }
+
+        return $response->setData([
+            'message' => $message
+        ]);
     }
 }
