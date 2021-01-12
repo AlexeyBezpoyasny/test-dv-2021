104f4a78fe30b85f410a6783b4679af9135a4053
Oleksii Bezpoiasnyi <abezpoasnyj@gmail.com>
2021-01-13 01:42:36 +0200

    #13 Cookies. Session. Session storage: implemented message display in the tab on the Product Page instead of the button when request has already been sent

--- app/code/OleksiiBezpoiasnyi/RegularCustomer/view/frontend/web/js/button.js
+++ app/code/OleksiiBezpoiasnyi/RegularCustomer/view/frontend/web/js/button.js
@@ -1,16 +1,23 @@
 define([
     'jquery',
-    'jquery/ui'
-], function ($) {
+    'jquery/ui',
+    'mage/translate',
+    'Magento_Ui/js/modal/alert'
+], function ($, alert) {
     'use strict';
 
     $.widget('oleksiib.regularCustomerButton', {
+        options: {
+            url: '',
+            messageWrap: '#oleksiib-regular-customer-message-wrap'
+        },
+
         /**
          * Constructor
          * @private
          */
         _create: function () {
-            $(this.element).click(this.openRegisterForm.bind(this));
+            $(this.element).click(this.ajaxRequest.bind(this));
         },
 
         /**
@@ -18,6 +25,48 @@ define([
          */
         openRegisterForm: function () {
             $(document).trigger('oleksiib_regular_customers_form_open');
+        },
+
+        /**
+         * Generate event to displayed message
+         */
+        displayedMessage: function () {
+            $(document).trigger('oleksiib_regular_customers_show_message');
+            $(this.element).css('display', 'none');
+        },
+
+        /**
+         * Submit request via AJAX. Add product id to the post data.
+         */
+        ajaxRequest: function () {
+            $.ajax({
+                url: this.options.url,
+                data: {
+                    'isAjax': 1
+                },
+                processData: false,
+                contentType: false,
+                type: 'post',
+                dataType: 'json',
+                context: this,
+
+                /** @inheritdoc */
+                success: function (response) {
+                    if (response.result === false) {
+                        this.openRegisterForm();
+                    } else {
+                        this.displayedMessage();
+                    }
+                },
+
+                /** @inheritdoc */
+                error: function () {
+                    alert({
+                        title: $.mage.__('Error'),
+                        content: $.mage.__('Your request can\'t be sent. Please, contact us if you see this message.')
+                    });
+                }
+            });
         }
     });
 
