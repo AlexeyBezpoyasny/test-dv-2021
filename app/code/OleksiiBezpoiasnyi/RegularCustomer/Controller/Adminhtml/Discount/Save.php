008bd99117e6371a8bf118036ad9ee916ec8f2b9
Oleksii Bezpoiasnyi <oleksiib@default-value.com>
2021-03-04 15:03:44 +0200

    #18 Listings and forms: implement saved requests in form and added dropdown in form to status

--- app/code/OleksiiBezpoiasnyi/RegularCustomer/Controller/Adminhtml/Discount/Save.php
+++ app/code/OleksiiBezpoiasnyi/RegularCustomer/Controller/Adminhtml/Discount/Save.php
@@ -3,28 +3,59 @@ declare(strict_types=1);
 
 namespace OleksiiBezpoiasnyi\RegularCustomer\Controller\Adminhtml\Discount;
 
-use OleksiiBezpoiasnyi\RegularCustomer\Model\Authorization;
 use Magento\Framework\Controller\ResultInterface;
+use OleksiiBezpoiasnyi\RegularCustomer\Model\Authorization;
+use OleksiiBezpoiasnyi\RegularCustomer\Model\DiscountRequest;
 
 class Save extends \Magento\Backend\App\Action implements \Magento\Framework\App\Action\HttpPostActionInterface
 {
     public const ADMIN_RESOURCE = Authorization::ACTION_DISCOUNT_REQUEST_EDIT;
 
+    private \OleksiiBezpoiasnyi\RegularCustomer\Model\DiscountRequestFactory $discountRequestFactory;
+
+    private \OleksiiBezpoiasnyi\RegularCustomer\Model\ResourceModel\DiscountRequest $discountRequestResource;
+
     /**
-     * @return ResultInterface
+     * Save constructor.
+     *
+     * @param \OleksiiBezpoiasnyi\RegularCustomer\Model\DiscountRequestFactory $discountRequestFactory
+     * @param \OleksiiBezpoiasnyi\RegularCustomer\Model\ResourceModel\DiscountRequest $discountRequestResource
+     * @param \Magento\Backend\App\Action\Context $context
      */
-    public function execute(): ResultInterface
+    public function __construct(
+        \OleksiiBezpoiasnyi\RegularCustomer\Model\DiscountRequestFactory $discountRequestFactory,
+        \OleksiiBezpoiasnyi\RegularCustomer\Model\ResourceModel\DiscountRequest $discountRequestResource,
+        \Magento\Backend\App\Action\Context $context
+    )
+    {
+        parent::__construct($context);
+        $this->discountRequestFactory = $discountRequestFactory;
+        $this->discountRequestResource = $discountRequestResource;
+    }
+
+    public function execute()
     {
         /** @var \Magento\Backend\Model\View\Result\Redirect $resultRedirect */
         $resultRedirect = $this->resultRedirectFactory->create();
+        try {
+            /** @var DiscountRequest $discountRequest */
+            $discountRequest = $this->discountRequestFactory->create();
 
-        // @TODO: save request if changed
+            $discountRequest->setProductId($this->getRequest()->getParam('product_id'))
+                ->setName($this->getRequest()->getParam('name'))
+                ->setEmail($this->getRequest()->getParam('email'))
+                ->setWebsiteId($this->getRequest()->getParam('website_id'))
+                ->setStatus($this->getRequest()->getParam('status'));
+
+            $this->messageManager->addSuccessMessage(__('Request saved!'));
+
+            $this->discountRequestResource->save($discountRequest);
+        } catch (\Exception $e) {
+            $this->messageManager->addErrorMessage($e->getMessage());
+        }
 
         return $resultRedirect->setPath(
-            '*/*/edit',
-            [
-                'request_id' => $this->getRequest()->getParam('request_id')
-            ]
+            '*/*/'
         );
     }
 }
